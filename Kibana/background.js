7782dba8b2f82e2e83fc374a05b59dcf41ecf0b5
wenjie.xu <wenjie.xu@bianlifeng.com>
2019-01-02 10:52:28 +0800

    使用override功能，替换书签页面

--- Kibana/background.js
+++ Kibana/background.js
@@ -5,5 +5,5 @@ const decode = (string) => {
     return string.split('').map(char => char.charCodeAt(0)).map(code => String.fromCharCode(code - 1)).join('');
 }
 chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.create({})
+    chrome.tabs.create({ url: 'chrome://bookmarks' })
 });
