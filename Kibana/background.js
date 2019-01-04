824321fb397290666c3f16b7fadd4d4321c4043b
Kyrie Ving <834854385@qq.com>
2019-01-04 21:55:41 +0800

    clear

--- Kibana/background.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-chrome.browserAction.onClicked.addListener(function () {
-    const path = chrome.runtime.getURL('./release/index.html');
-    chrome.tabs.create({ url: path });
-});
