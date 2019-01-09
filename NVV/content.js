4ff23314fb7cc566f163d74548ef738cac8a5ea1
Kyrie Ving <834854385@qq.com>
2019-01-09 22:57:33 +0800

    iqiyi音频没搞定

--- NVV/content.js
+++ NVV/content.js
@@ -1,16 +1,49 @@
 
-const aList = document.querySelectorAll('span.item a');
-const getCurUrl = () => window.location.href;
 
-Array.from(aList).forEach(node => {
-    node.onclick = function (e) {
-        location.assign(node.href)
-    }
-});
-const vipUrl = "https://jx.wslmf.com/?url=" + getCurUrl();
+const getCurUrl = () => location.href;
 
-const i = document.getElementById('mod_player');
-i.innerHTML = `<iframe id="mainIframe" height="100%" width="100%" name="mainIframe" src=${vipUrl} frameborder="0" scrolling="auto" ></iframe>`
+
+
+
+
+const [i, q] = ['www.iqiyi.com', 'v.qq.com']
+
+if (getCurUrl().includes(q)) {
+    qq();
+}
+if (getCurUrl().includes(i)) {
+    iqiyi();
+}
+
+
+
+function qq() {
+    const aList = document.querySelectorAll('span.item a');
+
+    Array.from(aList).forEach(node => {
+        node.onclick = function (e) {
+            location.assign(node.href)
+        }
+    });
+    const vipUrl = "https://jx.wslmf.com/?url=" + getCurUrl();
+
+    const i = document.getElementById('mod_player');
+    i.innerHTML = `<iframe id="mainIframe" height="100%" width="100%" name="mainIframe" src=${vipUrl} frameborder="0" scrolling="auto" ></iframe>`
+}
+function iqiyi() {
+    alert('iqiyi')
+    const aList = document.querySelectorAll('.select-link');
+    console.log(aList)
+    Array.from(aList).forEach(node => {
+        node.onclick = function (e) {
+            location.assign(node.baseURI.replace('https://' + i, ''))
+        }
+    });
+
+    const vipUrl = "https://jx.wslmf.com/?url=" + getCurUrl();
+    const i = document.getElementById('flashbox');
+    i.innerHTML = `<iframe id="mainIframe" height="100%" width="100%" name="mainIframe" src=${vipUrl} frameborder="0" scrolling="auto" ></iframe>`
+}
 
 
 
